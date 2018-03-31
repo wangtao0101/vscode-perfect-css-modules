4b1c51c33b3817178cfb635105826c7c058754a0
wangtao0101 <yuecjn@gmail.com>
2018-03-31 17:11:28 +0800

    support import from npm_modules

--- src/less/lessImportPlugin.ts
+++ src/less/lessImportPlugin.ts
@@ -2,6 +2,8 @@ const less = require('less');
 import * as path from 'path';
 import * as fs from 'fs';
 
+const isModuleName = /^~[^/\\]+$/;
+
 export default function LessImportPlugin() {
 
     class ImportFileManager extends less.FileManager {
@@ -13,9 +15,22 @@ export default function LessImportPlugin() {
             return false;
         }
 
-        loadFile(filename, currentDirectory, options) {
-            // TODO: resolve filename in nodemodules
-            const name = path.join(currentDirectory, filename);
+        loadFile(filename: string, currentDirectory, options) {
+            let url: string;
+            const isNpm = filename.startsWith('~');
+            if (options.ext && !isModuleName.test(filename)) {
+                url = this.tryAppendExtension(filename, options.ext);
+            } else {
+                url = filename;
+            }
+
+            let name;
+            if (isNpm) {
+                name = path.join(options.rootpath, 'node_modules' ,url.substr(1));
+            } else {
+                name = path.join(currentDirectory, url);
+            }
+
             if (fs.existsSync(name)) {
                 const data = fs.readFileSync(name, 'utf-8');
                 return Promise.resolve({
--- test/workspace/src/index.ts
+++ test/workspace/src/index.ts
@@ -1,2 +1,6 @@
 import * as a from './index.less';
 import * as b from './usevariable.less';
+
+a.aaaDdd
+a.a
+a.banner
